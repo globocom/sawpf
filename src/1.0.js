cb22ec728fe51b889267e0f233f86809faedb7f0
Fabio M. Costa <fabiomcosta@gmail.com>
2010-10-06 02:14:42 -0300

    Diminuindo o tamanho do arquivo js e colocando todas as imagens em um sprite só

--- src/1.0.js
+++ src/1.0.js
@@ -1,4 +1,4 @@
-(function(window, document, undefined){
+(function(window, document, navigator, undefined){
 
   // emile.js (c) 2009 Thomas Fuchs
   // Licensed under the terms of the MIT license.
@@ -51,12 +51,16 @@
   })('emile', this);
 
   // ---------------------------------------------------------------------------------------------------
+  
 
+  var userAgent = navigator.userAgent;
+  var vendor = navigator.vendor;
+  var platform = navigator.platform;
+  
   var BrowserDetect = { // BrowserDetect adapted from http://www.quirksmode.org/js/detect.html
     /** @constructor */ init: function () {
       this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
-      this.version = this.searchVersion(navigator.userAgent) || this.searchVersion(navigator.appVersion) || -1;
-      this.OS = this.searchString(this.dataOS) || "an unknown OS";
+      this.version = this.searchVersion(userAgent) || this.searchVersion(navigator.appVersion) || -1;
     },
     /** @constructor */ searchString: function (data) {
       for (var i=0; i<data.length; i++)  {
@@ -75,54 +79,50 @@
       return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
     },
     dataBrowser: [
-      {string: navigator.userAgent, subString: 'Chrome', identity: 'Chrome'},
-      {string: navigator.userAgent, subString: 'OmniWeb', versionSearch: 'OmniWeb/', identity: 'OmniWeb'},
-      {string: navigator.vendor, subString: 'Apple', identity: 'Safari', versionSearch: 'Version'},
+      {string: userAgent, subString: 'Chrome', identity: 'Chrome'},
+      {string: userAgent, subString: 'OmniWeb', versionSearch: 'OmniWeb/', identity: 'OmniWeb'},
+      {string: vendor, subString: 'Apple', identity: 'Safari', versionSearch: 'Version'},
       {prop: window.opera, identity: 'Opera'},
-      {string: navigator.vendor, subString: 'iCab', identity: 'iCab'},
-      {string: navigator.vendor, subString: "KDE", identity: "Konqueror"},
-      {string: navigator.userAgent, subString: "Firefox", identity: "Firefox"},
-      {string: navigator.vendor, subString: "Camino", identity: "Camino"},
-      {string: navigator.userAgent, subString: "Netscape", identity: "Netscape"}, // for newer Netscapes (6+)
-      {string: navigator.userAgent, subString: "MSIE", identity: "Explorer", versionSearch: "MSIE"},
-      {string: navigator.userAgent, subString: "Gecko", identity: "Mozilla", versionSearch: "rv"},
-      {string: navigator.userAgent, subString: "Mozilla", identity: "Netscape", versionSearch: "Mozilla"}// for older Netscapes (4-)
-    ],
-    dataOS : [
-      {string: navigator.platform, subString: "Win", identity: "Windows"},
-      {string: navigator.platform, subString: "Mac", identity: "Mac"},
-      {string: navigator.userAgent, subString: "iPhone", identity: "iPhone/iPod"},
-      {string: navigator.platform, subString: "Linux", identity: "Linux"}
+      {string: vendor, subString: 'iCab', identity: 'iCab'},
+      {string: vendor, subString: "KDE", identity: "Konqueror"},
+      {string: userAgent, subString: "Firefox", identity: "Firefox"},
+      {string: vendor, subString: "Camino", identity: "Camino"},
+      {string: userAgent, subString: "Netscape", identity: "Netscape"}, // for newer Netscapes (6+)
+      {string: userAgent, subString: "MSIE", identity: "Explorer", versionSearch: "MSIE"},
+      {string: userAgent, subString: "Gecko", identity: "Mozilla", versionSearch: "rv"},
+      {string: userAgent, subString: "Mozilla", identity: "Netscape", versionSearch: "Mozilla"}// for older Netscapes (4-)
     ]
   };
   BrowserDetect.init();
-
   // ---------------------------------------------------------------------------------------------------
+  var isFirefox = (BrowserDetect.browser == 'Firefox');
+  var isIE = (BrowserDetect.browser == 'Explorer');
+  
+  if (!(isFirefox || isIE)) return;
+  if (isFirefox && BrowserDetect.version >= 3.5) return;
+  if (isIE && BrowserDetect.version >= 7) return;
 
-  if (!(BrowserDetect.browser == 'Firefox' || BrowserDetect.browser == 'Explorer')) return;
-  if (BrowserDetect.browser == 'Firefox' && BrowserDetect.version >= 3.5) return;
-  if (BrowserDetect.browser == 'Explorer' && BrowserDetect.version >= 7) return;
+  var SPRITE_URL = (window['base_url'] || 'http://sawpf.com') + '/sprite.gif';
 
-  var BASE_URL = window['base_url'] || 'http://sawpf.com';
-  var IE_BUTTON = '<a href="http://www.baixatudo.com.br/internet-explorer-8?utm_source=sawpf&utm_medium=banner&utm_campaign=Explorer" class="sawpf-ie" title="Internet Explorer 8"><img src="' + BASE_URL + '/ie.gif" alt="Internet Explorer"/></a></li>';
-  var FIREFOX_BUTTON = '<a href="http://www.baixatudo.com.br/mozilla-firefox-3-6?utm_source=sawpf&utm_medium=banner&utm_campaign=Firefox" class="sawpf-ff" title="Firefox 3.6"><img src="' + BASE_URL + '/ff.gif" alt="Firefox"/></a></li>';
-  var CHROME_BUTTON = '<a href="http://www.baixatudo.com.br/google-chrome-4?utm_source=sawpf&utm_medium=banner&utm_campaign=Chrome" class="sawpf-gc" title="Google Chrome 4"><img src="' + BASE_URL + '/gc.gif" alt="Google Chrome"/></a></li>';
-
-  var css = '#sawpf * {margin: 0; padding: 0; top: 0; line-height: 1em;}\n';
-  css += '#sawpf {text-align: center; height: 0; overflow: hidden; background: #ffffd6; border-bottom: 1px solid #f0e4c3; border-top: 1px solid #f0e4c3; font-family: arial; margin: 0; position: relative; width: 100%;}\n';
-  css += '#sawpf div {text-align: left; margin: 0 auto; width: 940px; padding: 9px 0;}\n';
-  css += '#sawpf strong {color: #333; font-size: 14px;}\n';
-  css += '#sawpf p {color: #666; float: left; font-size: 12px; line-height: 18px; margin: 2px 20px 0 0; text-align: left;}\n';
-  css += '#sawpf ul {list-style: none}\n';
-  css += '#sawpf li {display: block; float: left; list-style: none; margin: 0 5px 0 0;}\n';
-  css += '#sawpf img {border: none;}\n';
-  css += '#sawpf a {border: none; background: #fff url(' + BASE_URL + '/sprite-bt.gif) no-repeat 0 0; display: block; height: 40px; outline: none; overflow: hidden; width: 150px;}\n';
-  css += '#sawpf a:hover {background-position: 0 -40px;}\n';
-  css += '#sawpf a:active {background-position: 0 -80px;}\n';
-  css += '#sawpf a:active img {margin-top: 1px;}\n';
-  css += '#sawpf #sawpf-close {background: url(' + BASE_URL + '/bt-close.gif) no-repeat 0 0; height: 15px; position: absolute; right: 5px; text-indent: -99999px; top: 5px; width: 15px;}\n';
-  css += '#sawpf #sawpf-close:hover {background-position: 0 -15px;}\n';
-  css += '#sawpf #sawpf-close:active {background-position: 0 -30px;}\n';
+  var css = '#sawpf * {margin: 0; padding: 0}' +
+    '#sawpf {text-align: center; height: 0; overflow: hidden; background: #ffffd6; border-width: 1px 0; border-color: #f0e4c3; border-style: solid; font-family: arial; position: relative; top: 0; width: 100%}' +
+    '#sawpf div {margin: 0 auto; width: 940px; padding: 9px 0}' +
+    '#sawpf strong {color: #333; font-size: 14px}' +
+    '#sawpf p {color: #666; float: left; font-size: 12px; line-height: 18px; margin: 2px 20px 0 0; text-align: left}' +
+    '#sawpf ul {list-style: none}' +
+    '#sawpf li {display: block; float: left; margin-right: 5px}' +
+    '#sawpf a, #sawpf a span {background-image: url(' + SPRITE_URL + '); text-indent: -99em; display: block; cursor: pointer}' +
+    '#sawpf a {outline: none; overflow: hidden}' +
+    '#sawpf ul a, #sawpf ul a span {height: 40px; width: 150px}' +
+    '#sawpf ul a:hover {background-position: 0 -40px}' +
+    '#sawpf ul a:active {background-position:0 -80px}' +
+    '#sawpf ul a:active span {margin-top: 1px}' +
+    '#sawpf .sawpf-ie span {background-position: 0 -198px}' +
+    '#sawpf .sawpf-ff span {background-position: 0 -120px}' +
+    '#sawpf .sawpf-gc span {background-position: 0 -158px}' +
+    '#sawpf #sawpf-close {background-position: 0 -240px; width: 15px; height: 15px; position: absolute; right:5px; top: 5px}' +
+    '#sawpf #sawpf-close:hover {background-position: 0 -255px}' +
+    '#sawpf #sawpf-close:active {background-position: 0 -270px}';
 
   var styleTag = document.createElement('style');
   styleTag.type = 'text/css';
@@ -133,21 +133,18 @@
   }
   document.getElementsByTagName('head')[0].appendChild(styleTag);
 
-  var html = '<div id="sawpf"><div><p><strong>Seu ';
-  html += (BrowserDetect.browser == 'Explorer') ? "Internet Explorer" : "Firefox";
-  html += ' está desatualizado.</strong><br/>Para uma melhor visualização do site atualize-o ou escolha outro navegador.</p>';
-  html += '<ul>';
-  if(BrowserDetect.browser == 'Explorer') {
-    html += '<li>' + IE_BUTTON + '</li>';
-    html += '<li>' + FIREFOX_BUTTON + '</li>';
-  } else {
-    html += '<li>' + FIREFOX_BUTTON + '</li>';
-    html += '<li>' + IE_BUTTON + '</li>';
-  }
-  html += '<li>' + CHROME_BUTTON + '</li>';
-  html += '</ul>';
-  html += '<a href="#" id="sawpf-close" title="Fechar">fechar</a>';
-  html += '</div></div>';
+  var IE_BUTTON = '<a href="http://www.baixatudo.com.br/internet-explorer?utm_source=sawpf&utm_medium=banner&utm_campaign=Explorer" class="sawpf-ie" title="Internet Explorer"><span>Internet Explorer</span></a>';
+  var FIREFOX_BUTTON = '<a href="http://www.baixatudo.com.br/mozilla-firefox?utm_source=sawpf&utm_medium=banner&utm_campaign=Firefox" class="sawpf-ff" title="Firefox"><span>Firefox</span></a>';
+  var CHROME_BUTTON = '<a href="http://www.baixatudo.com.br/google-chrome?utm_source=sawpf&utm_medium=banner&utm_campaign=Chrome" class="sawpf-gc" title="Google Chrome"><span>Google Chrome</span></a>';
+
+  var html = '<div id="sawpf"><div><p><strong>Seu ' +
+    ((isIE) ? "Internet Explorer" : BrowserDetect.browser) +
+    ' está desatualizado.</strong><br/>Para uma melhor visualização do site atualize-o ou escolha outro navegador.</p>' +
+    '<ul><li>' +
+    FIREFOX_BUTTON + '</li><li>' + IE_BUTTON + '</li><li>' + CHROME_BUTTON +
+    '</li></ul>' +
+    '<a href="#" id="sawpf-close" title="Fechar">fechar</a>' +
+    '</div></div>';
 
   var container = document.createElement('div');
   container.innerHTML = html;
@@ -167,5 +164,5 @@
     return false;
   };
   emile('sawpf', 'height: 58px', {duration: 500});
-})(this, document);
+})(this, document, navigator);
 
