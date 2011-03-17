ac5aab76dbf01c2c077927a910c7c41d3090b949
Jose Peleteiro <jose@peleteiro.net>
2011-03-17 00:07:02 -0300

    FIX descrição do teste

--- spec/compatible_mode_ie7_spec.js
+++ spec/compatible_mode_ie7_spec.js
@@ -2,7 +2,7 @@ fakeUserAgent('Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Trident/4.0)')
 document['documentMode'] = 5;
 require('/src/1.0.js');
 
-describe('Internet Explorer 8.0 com user-agent duplo com o IE6', function() {
+describe('Internet Explorer 8.0/9.0 com user-agent do IE7 por causa do compatibility mode', function() {
   it('should not show sawpf bar', function() {
     expect(document.getElementById('sawpf')).not.toExist();
   });
