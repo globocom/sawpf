c76fca68a623e1eb15a90d117868af09cafc85c7
Jose Peleteiro <jose@peleteiro.net>
2011-03-21 10:34:01 -0300

    Faltou commitar os testes

--- spec/ff35_spec.js
+++ spec/ff35_spec.js
@@ -2,7 +2,7 @@ fakeUserAgent('Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.8) Gecko/20100
 require('/src/1.0.js');
 
 describe('Firefox 3.5', function() {
-  it('should not show sawpf bar', function() {
-    expect(document.getElementById('sawpf')).not.toExist();
+  it('should show sawpf bar', function() {
+    expect(document.getElementById('sawpf')).toExist();
   });
 });
---
+++ spec/ff40_spec.js
@@ -0,0 +1,9 @@
+fakeUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0) Gecko/20100101 Firefox/4.0,gzip(gfe)');
+require('/src/1.0.js');
+
+describe('Firefox 4.0', function() {
+  it('should not show sawpf bar', function() {
+    expect(document.getElementById('sawpf')).not.toExist();
+  });
+});
+
