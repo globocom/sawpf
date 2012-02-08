e3bd1231145a321a8f81b027cd0d6f31252da559
Jose Peleteiro <jose@peleteiro.net>
2012-02-08 17:17:38 -0200

    primeiro teste de multiplos browsers

--- spec/ff90_spec.js
+++ spec/ff90_spec.js
@@ -1,10 +1,34 @@
 Cookie.unset('__sawpf_');
-fakeUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0a2) Gecko/20111101 Firefox/9.0a2');
-require('/src/1.0.js');
+
+function loadSawpf() {
+  (function(d,t){
+    var g=d.createElement(t),
+    s=d.getElementsByTagName(t)[0];
+    g.src='/src/1.0.js';
+    s.parentNode.insertBefore(g,s)
+  }(document,'script'));
+}
 
 describe('Firefox 9.0', function() {
+
+  beforeEach(function() {
+    return;
+    var el = document.getElementById('sawpf');
+    if (!el) return;
+    el.parent.removeChild(el);
+  });
+
   it('should not show sawpf bar', function() {
+    fakeUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0a2) Gecko/20111101 Firefox/9.0a2');
+    loadSawpf();
     expect(document.getElementById('sawpf')).not.toExist();
   });
+
+  it('should not show sawpf bar', function() {
+    fakeUserAgent('Mozilla/5.0 (Windows NT 6.2; rv:9.0.1) Gecko/20100101 Firefox/9.0.1');
+    loadSawpf();
+    expect(document.getElementById('sawpf')).toExist();
+  });
+
 });
 
